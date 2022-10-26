1a5c2972cc6eb82fd08727a73ff077087107a249
Kelbas Pavlo <114227096+PavelJH@users.noreply.github.com>
2022-10-26 18:32:34 +0200

    Add files via upload

---
+++ Java_HW_26.10.2022/src/main/java/HW.java
@@ -0,0 +1,12 @@
+public class HW {
+    public static void main(String[] args) {
+        int count = 0;
+        int et = 0;
+        while (et <= 163) {
+            et += 4;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
+
